5bae5f45aaec9600aa31b46193937354790db271
App Academy Student <student@appacademy.io>
2014-09-26 12:00:00 -0700

    more into messages

--- app/assets/javascripts/views/message/index.js
+++ app/assets/javascripts/views/message/index.js
@@ -3,11 +3,32 @@ PictureApp.Views.MessageIndex = Backbone.View.extend({
 	
 	events: {
 		'click #close-messages' : 'closeMessages',
-		'click .delete-message' : 'deleteMessage'
+		'click .delete-message' : 'deleteMessage',
+		'click #send-message' : 'sendMessage',
 	},
 	
 	deleteMessage: function () {
 		event.preventDefault();
+	},
+	
+	sendMessage: function () {
+		event.preventDefault();
+		var messageBody = $('#message-body').val();
+		var messageRecipient = $('#message-recipient').val();
+		var messageSubject = $('#message-subject').val();
+		var sender = $('#current-user-id').data('current-user-id');
+		var newMessage = new PictureApp.Models.SentMessage({
+			message: {body: messageBody, 
+				sender_id: sender, 
+				sendee_id: messageRecipient,
+				title: messageSubject
+			}
+		});
+		newMessage.save({}, {
+			success: function() {
+				alert('success!')
+			}
+		})
 	},
 	
 	initialize: function (options) {
@@ -19,8 +40,12 @@ PictureApp.Views.MessageIndex = Backbone.View.extend({
 	
 	handleToggle: function (event) {
 		event.preventDefault();
+		debugger
 		var desiredContent = $(event.currentTarget).data('name');
 		$('.middle-pane').removeClass('being-viewed');
+		if ($(event.currentTarget).hasClass('for-message')) {
+
+		}
 		$(desiredContent).addClass('being-viewed');
 	},
 	
